2924dc34b6d21b91b004f6b7abd7639a635ffaa9
jivandandapat07 <jivandandapat07@gmail.com>
2023-08-15 12:51:37 +0530

    This is my hollow rectangle pattern code

---
+++ hollow_rec_patteren.java
@@ -0,0 +1,19 @@
+public class hollow_rec_patteren {
+    public static void hollow_rectangle(int totRows , int totCols){
+        for(int i=1; i<=totRows; i++){
+            for(int j=1; j<=totCols; j++){
+                if(i == 1 || i == 4 || j == 1 || j == 5){
+                    System.out.print("*");
+                }else{
+                     System.out.print(" ");
+                }
+            }
+             System.out.println();
+        }
+    }
+
+     public static void main(String args[]){
+        hollow_rectangle(4, 5);
+     }
+    
+}
