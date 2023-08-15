d787871b78cf68057307e2219eed15c7988e3a93
jivandandapat07 <jivandandapat07@gmail.com>
2023-08-15 13:01:30 +0530

    This is my Butterfly pattern Program

---
+++ Butterfly_Pattern.java
@@ -0,0 +1,41 @@
+// Butterfly Pattern
+
+public class pattern_8 {
+    public static void butterfly_pattern(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+  
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+  
+            System.out.println();
+        }
+    }
+        
+    
+
+     public static void main(String args[]){
+        butterfly_pattern(5);
+     }
+}
